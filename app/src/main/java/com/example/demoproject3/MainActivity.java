e572790fea109da6d8900ffb04ead7a49f72ffb4
Anisur <anisur.int@orangetoolz.com>
2021-09-13 19:07:42 +0600

    add ui

--- app/src/main/java/com/example/demoproject3/MainActivity.java
+++ app/src/main/java/com/example/demoproject3/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import okhttp3.ResponseBody;
@@ -15,13 +18,28 @@ public class MainActivity extends AppCompatActivity {
      private static final String clientId = "admin-cli";
      private static final String clientSecret = "0406a491-3e16-46a5-8108-cbe73fa16c11";
      private static final String grantType = "password";
+
+     Button login;
+     EditText username,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String userName = "res";
-        String password = "res";
-        executeRequest(userName,password,clientId,clientSecret,grantType);
+        username = findViewById(R.id.usernameId);
+        pass = findViewById(R.id.passwordId);
+        login = findViewById(R.id.loginId);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = username.getText().toString();
+                String password = pass.getText().toString();
+                if(!userName.isEmpty() && !password.isEmpty()){
+                    executeRequest(userName,password,clientId,clientSecret,grantType);
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter username and password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
     public void executeRequest(String userName,String password, String clientId, String clientSecret, String grantType){
         APIClient apiClient = RetrofitRest.getRetroFit().create(APIClient.class);
@@ -33,15 +51,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                if(response.code()==200){
-                   Log.d("anisur","success");
+                   Toast.makeText(MainActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
                } else if(response.code() == 401){
-                   Log.d("anisur","unauthorized");
+                   Toast.makeText(MainActivity.this, "Invalid username or password!", Toast.LENGTH_SHORT).show();
                }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "connection failed!", Toast.LENGTH_SHORT).show();
             }
         });
     }
